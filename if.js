20cde582765b9c07ea36544434b9186b4890fccc
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-15 14:55:45 +0530

    create user loggin

---
+++ if.js
@@ -0,0 +1,9 @@
+//if else - conditional statement
+
+pwd_correct = true;
+if (pwd_correct) {
+    console.log("You are logging in")
+} else
+    console.log("your password incorrect")
+
+console.log("Try Again")
