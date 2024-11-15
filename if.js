ede1d701a87dd483e359071bae131a2cedbeed47
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-15 22:48:35 +0530

    if.js

--- if.js
+++ if.js
@@ -7,3 +7,16 @@ if (pwd_correct) {
     console.log("your password incorrect")
 
 console.log("Try Again")
+
+//Operators
+//Arithmetic operators 
+a = 10;
+b = 20;
+
+console.log(a + b)
+console.log(a - b)
+console.log(a % b) //module means remainder
+console.log(a / b)
+console.log(a * b)
+console.log(a ** 2)
+console.log(a * b + a - b) //a*(b+a)-a
